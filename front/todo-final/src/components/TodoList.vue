95e654ce2aeaf07e08385c6a21f264b88709a8da
dynii <dyniiyeyo@naver.com>
2025-11-18 18:05:04 +0900

    251118 lecture

--- front/todo-final/src/components/TodoList.vue
+++ front/todo-final/src/components/TodoList.vue
@@ -1,7 +1,5 @@
 <script>
 export default {
-  // 부모 컴포넌트에서 전달한 computedTodo 데이터를
-  // TodoList컴포넌트에서 props 옵션 속성으로 받는다
   props: {
     computedTodo: {
       type: Array,
@@ -10,22 +8,18 @@ export default {
       },
     },
   },
-
   methods: {
     deleteTodo(id) {
-      this.$emit('delete-todo', id);
+      this.$emit("delete-todo", id);
     },
     updateTodo(id) {
-      this.$emit('update-todo', id);
+      this.$emit("update-todo", id);
     },
   },
 };
 </script>
-
 <template>
   <div class="todo__list">
-    <!-- 할 일 목록이 있을 때 (완료 시 .todo__item--completed 클래스 추가 )-->
-
     <div
       v-for="item in computedTodo"
       :key="item.id"
@@ -46,16 +40,11 @@ export default {
       <span
         class="material-symbols-outlined todo__delete-icon"
         @click="deleteTodo(item.id)"
-        >delete
-      </span>
+        >delete</span
+      >
     </div>
-    <!-- 할 일 목록이 없을 때 -->
-    <div
-      v-if="computedTodo.length === 0"
-      class="todo__item--no"
-      style="display: none"
-    >
-      <p>할일 목록이 없습니다.</p>
+    <div v-if="computedTodo.length === 0" class="todo__item--no">
+      <p>할 일이 없습니다.</p>
     </div>
   </div>
 </template>
