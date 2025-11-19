ad4baf779e7e829a2a762102adeace2fa679d14a
dynii <dyniiyeyo@naver.com>
2025-11-19 20:19:47 +0900

    251119 assignment

--- front/day03_02/src/App.vue
+++ front/day03_02/src/App.vue
@@ -1,7 +1,6 @@
 <script>
 import practice1 from './components/practice1.vue';
 import practice2 from './components/practice2.vue';
-import practice2Child from './components/practice2Child.vue';
 import practice3 from './components/practice3.vue';
 import practice4 from './components/practice4.vue';
 import practice5 from './components/practice5.vue';
@@ -9,8 +8,7 @@ import practice5 from './components/practice5.vue';
 export default {
   components: {
     // practice1,
-    practice2,
-    // practice2Child,
+    // practice2,
     // practice3,
     // practice4,
     // practice5,
@@ -19,8 +17,7 @@ export default {
 </script>
 <template>
   <!-- <practice1 /> -->
-  <practice2 />
-  <!-- <practice2Child /> -->
+  <!-- <practice2 /> -->
   <!-- <practice3 /> -->
   <!-- <practice4 /> -->
   <!-- <practice5 /> -->
