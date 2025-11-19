bb0a1f174965e5594cd6f95a10da1fca89a581ae
dynii <dyniiyeyo@naver.com>
2025-11-19 15:31:01 +0900

    251119 lecture

---
+++ front/day03_01/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import DataEx1 from './components/DataEx1.vue';
+import DataEx2 from './components/DataEx1.vue';
+import DataEx3 from './components/DataEx1.vue';
+import DataEx4 from './components/DataEx1.vue';
+import RefEx1 from './components/RefEx1.vue';
+
+export default {
+  components: {
+    // DataEx1,
+    // DataEx2,
+    // DataEx3,
+    DataEx4,
+    RefEx1,
+  },
+};
+</script>
+<template>
+  <!-- <DataEx1 />
+  <DataEx2 />
+  <DataEx3 /> -->
+  <DataEx4 />
+  <RefEx1 />
+</template>
---
+++ front/day03_01/src/components/DataEx2.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue';
+
+const arr1 = ref([10]); // 배열로 반응형 데이터 생성
+const arr2 = ref([10]);
+
+console.log(count.value);
+console.log(state.value.count);
+
+setTimeout(() => {
+  arr1.value = [10, 20]; // 배열의 값 자체를 변경
+}, 1000);
+
+setTimeout(() => {
+  arr2.push(20); // 배열의 요소 변경(추가)
+}, 1000);
+</script>
+
+<template>
+  <h1>count : {{ arr1 }}</h1>
+  <h1>state : {{ arr2 }}</h1>
+</template>
