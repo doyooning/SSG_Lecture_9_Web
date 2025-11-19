f4f58aba3a54a8e0277cb4b16df450468da4b4ac
dynii <dyniiyeyo@naver.com>
2025-11-19 16:12:54 +0900

    251119 lecture

--- front/day03_01/src/App.vue
+++ front/day03_01/src/App.vue
@@ -1,17 +1,23 @@
 <script>
-import DataEx1 from './components/DataEx1.vue';
-import DataEx2 from './components/DataEx1.vue';
-import DataEx3 from './components/DataEx1.vue';
-import DataEx4 from './components/DataEx1.vue';
-import RefEx1 from './components/RefEx1.vue';
+// import DataEx1 from './components/DataEx1.vue';
+// import DataEx2 from './components/DataEx2.vue';
+// import DataEx3 from './components/DataEx3.vue';
+// import DataEx4 from './components/DataEx4.vue';
+// import RefEx1 from './components/RefEx1.vue';
+// import RefEx2 from './components/RefEx2.vue';
+import ComputedEx1 from './components/ComputedEx1.vue';
+import ComputedEx2 from './components/ComputedEx2.vue';
 
 export default {
   components: {
     // DataEx1,
     // DataEx2,
     // DataEx3,
-    DataEx4,
-    RefEx1,
+    // DataEx4,
+    // RefEx1,
+    // RefEx2,
+    ComputedEx1,
+    ComputedEx2,
   },
 };
 </script>
@@ -19,6 +25,9 @@ export default {
   <!-- <DataEx1 />
   <DataEx2 />
   <DataEx3 /> -->
-  <DataEx4 />
-  <RefEx1 />
+  <!-- <DataEx4 /> -->
+  <!-- <RefEx1 /> -->
+  <!-- <RefEx2 /> -->
+  <ComputedEx1 />
+  <ComputedEx2 />
 </template>
